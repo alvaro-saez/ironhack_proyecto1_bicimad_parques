bfe9a9f8156ca52e42f472a7247462ea795fcb64
alvaro-saez <alvarosaezsanchez@gmail.com>
2022-01-17 18:49:58 +0100

    lo basico terminado p1

---
+++ p_reporting/p_reporting.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+from shapely.geometry import Point
+import geopandas as gpd   # conda install -c conda-forge geopandas
+import numpy as np
+import requests
+
+import warnings
+#warnings.filterwarnings('ignore');
+pd.set_option('display.max_rows', 500)
+
+import email, smtplib, ssl
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+# #### importar datasets finales
+
+# In[2]:
+
+
+#location_min_dataset = "../datasets/final_df_min_distance_optimizated.csv"
+def import_min_dataset(location_min_dataset):
+    interaction_min_dataset = pd.read_csv(location_min_dataset, sep=',', index_col=False)
+    return interaction_min_dataset
+
+
+# In[3]:
+
+
+#interaction_min_dataset = import_min_dataset(location_min_dataset)
+#interaction_min_dataset
+
+
+# In[4]:
+
+
+def bicimad_station(input_parque,interaction_min_dataset):
+    bicimad_station = "la estación de bicis más cercana es: " +str(interaction_min_dataset.loc[(interaction_min_dataset[("Place of interest")]==input_parque), "BiciMAD station"].tolist()[0])
+    return bicimad_station
+
+
+# In[5]:
+
+
+def bicimad_adress(input_parque,interaction_min_dataset):
+    bicimad_adress = "su dirección es: " + str(interaction_min_dataset.loc[(interaction_min_dataset[("Place of interest")]==input_parque), "Station location"].tolist()[0])
+    return bicimad_adress
+
+
+# In[6]:
+
+
+def bicimad_bikes(input_parque,interaction_min_dataset):
+    bicimad_bikes = "hay " + str(interaction_min_dataset.loc[(interaction_min_dataset[("Place of interest")]==input_parque), "Station Bikes Availability"].tolist()[0]) + " bicis disponibles"
+    return bicimad_bikes
+
+
+# In[7]:
+
+
+def bicimad_dejar_bici(input_parque,interaction_min_dataset):
+    bicimad_dejar_bici =str(interaction_min_dataset.loc[(interaction_min_dataset[("Place of interest")]==input_parque), "Station base availability"].tolist()[0]) + " base para dejar la bici"
+    return bicimad_dejar_bici
+
+
+# In[8]:
+
+
+def place_description(input_parque,interaction_min_dataset):
+    place_description = str(interaction_min_dataset.loc[(interaction_min_dataset[("Place of interest")]==input_parque), "Place description"].tolist()[0])
+    return place_description
+
+
+# In[9]:
+
+
+def place_barrio(input_parque,interaction_min_dataset):
+    place_barrio = "su barrio es: " + str(interaction_min_dataset.loc[(interaction_min_dataset[("Place of interest")]==input_parque), "Place neighborhood"].tolist()[0])
+    return place_barrio
+
+
+# In[10]:
+
+
+def place_transport(input_parque,interaction_min_dataset):
+    place_transport = "contiene los siguientes transportes: " + str(interaction_min_dataset.loc[(interaction_min_dataset[("Place of interest")]==input_parque), "Place transport"].tolist()[0])
+    if str(place_transport) == "contiene los siguientes transportes: nan":
+        place_transport = "lo siento, no hay transporte público cercano, anda un poco que te vendrá bien"
+    elif "Servicio Bicimad" in str(place_transport):
+        place_transport = place_transport.split("Servicio Bicimad")[0]
+    return place_transport
+
+
+# In[11]:
+
+
+def bicimad_station_meters(input_parque,interaction_min_dataset):
+    bicimad_station = "la estación de bicis más cercana esta a : " + str(interaction_min_dataset.loc[(interaction_min_dataset[("Place of interest")]==input_parque), "Distance Between BiciMAD station and Place of interest"].tolist()[0]) + " metros"
+    return bicimad_station
+
+
+# In[17]:
+
+
+def wikiparque_email_sender(receiver_email,filename):
+
+    subject = "WikiParque"
+    body = "Te adjuntamos en formato CSV la informacion más relevante del conjunto de Paruqes de la Comunidad de Madrid"
+    sender_email = "alvarosaezsanchez@gmail.com"
+    receiver_email = receiver_email
+    password = "jswfzrfqxpxibfzi"
+    
+    # Create a multipart message and set headers
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    message["Subject"] = subject
+    message["Bcc"] = receiver_email  # Recommended for mass emails
+    
+    # Add body to email
+    message.attach(MIMEText(body, "plain"))
+    
+    filename = filename
+    
+    # Open PDF file in binary mode
+    with open(filename, "rb") as attachment:
+        # Add file as application/octet-stream
+        # Email client can usually download this automatically as attachment
+        part = MIMEBase("application", "octet-stream")
+        part.set_payload(attachment.read())
+    
+    # Encode file in ASCII characters to send by email    
+    encoders.encode_base64(part)
+    
+    # Add header as key/value pair to attachment part
+    part.add_header(
+        "Content-Disposition",
+        f"attachment; filename= {filename}",
+    )     
+    
+    # Add attachment to message and convert message to string
+    message.attach(part)
+    text = message.as_string()
+
+    # Log in to server using secure context and send email
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, text)
+
+
+# In[18]:
+
+
+#receiver_email = input("escribe tu email: ")
+#filename = "../datasets/final_df_min_distance_optimizated.csv" 
+#wikiparque_email_sender(receiver_email,filename)
+
+
+# In[ ]:
+
+
+
+
---
+++ wikiparque.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[13]:
+
+
+import pandas as pd
+from shapely.geometry import Point
+import geopandas as gpd   # conda install -c conda-forge geopandas
+import numpy as np
+import requests
+import argparse
+import warnings
+#warnings.filterwarnings('ignore');
+pd.set_option('display.max_rows', 500)
+
+import email, smtplib, ssl
+
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+# In[14]:
+
+
+from p_reporting import p_reporting as rp
+    
+def argument_parser():
+    parser = argparse.ArgumentParser(description='comandos para obtener información de tus parques favoritos')
+    
+    parser.add_argument("-l", "--listado", help="listado con todos los nombres de los parques de la Comunidad de Madrid", action='store_true')
+    parser.add_argument("-bs", "--bicimad_station", help="nombre de la estación de BiciMAD más cercana", action='store_true')
+    parser.add_argument("-bm", "--bicimad_station_meters", help="distancia a la estación de BiciMAD más cercana", action='store_true')
+    parser.add_argument("-ba", "--bicimad_adress", help="dirección de la estación de BiciMAD más cercana", action='store_true')
+    parser.add_argument("-bb", "--bicimad_bikes", help="número de bicis disponibles", action='store_true')
+    parser.add_argument("-bd", "--bicimad_dejar_bici", help="base disponible para dejar la bici", action='store_true')
+    parser.add_argument("-pd", "--place_description", help="descripción del parque", action='store_true')
+    parser.add_argument("-pb", "--place_barrio", help="barrio en el que se encuentra el parque", action='store_true')
+    parser.add_argument("-pt", "--place_transport", help="transporte público cercano al parque", action='store_true')
+    parser.add_argument("-e", "--email", help="escribe 'wikiparque.py -e' para recibir el listado completo de parques y estaciones de BiciMAD en tu email", action='store_true')
+
+
+    args = parser.parse_args()
+    return args
+
+
+def main(arguments):
+    
+    location_min_dataset = "datasets/final_df_min_distance_optimizated.csv"
+    interaction_min_dataset = rp.import_min_dataset(location_min_dataset)
+    filename = "datasets/final_df_min_distance_optimizated.csv" 
+    
+    
+    
+    if not arguments.listado and  not arguments.bicimad_station and  not arguments.bicimad_adress and  not arguments.bicimad_bikes and  not arguments.bicimad_dejar_bici and  not arguments.place_description and  not arguments.place_barrio and  not arguments.place_transport and  not arguments.place_transport and  not arguments.email:
+        print("Hola, esta es una app informativa sobre los Parques Municipales de la excelentisima Comunidad de Madrid, presidida por nuestra dueña y señora AYUSO.\n Escribe: 'python wikiparque.py -h' para saber todo lo que podemos ofrecerte")
+        
+    elif arguments.listado:
+        print(interaction_min_dataset["Place of interest"])
+        print("\n")
+        print("Si quieres más información escribe uno de los comandos que te mostramos en 'python wikiparque.py -h'")
+           
+    elif arguments.bicimad_station:
+        input_parque = input("¿Cuál es tu parque favorito?: ")
+        bicimad_station2 = rp.bicimad_station(input_parque,interaction_min_dataset)
+        if input_parque in interaction_min_dataset["Place of interest"].tolist():
+            print(bicimad_station2)
+        else:
+            print("Parque mal escrito... busca en Google cachondo")
+            
+    elif arguments.bicimad_adress:
+        input_parque = input("¿Cuál es tu parque favorito?: ")
+        bicimad_adress2 = rp.bicimad_adress(input_parque,interaction_min_dataset)
+        if input_parque in interaction_min_dataset["Place of interest"].tolist():
+            print(bicimad_adress2)
+        else:
+            print("Parque mal escrito... busca en Google cachondo")
+            
+    elif arguments.bicimad_bikes:
+        input_parque = input("¿Cuál es tu parque favorito?: ")
+        bicimad_bikes2 = rp.bicimad_bikes(input_parque,interaction_min_dataset)
+        if input_parque in interaction_min_dataset["Place of interest"].tolist():
+            print(bicimad_bikes2)
+        else:
+            print("Parque mal escrito... busca en Google cachondo")
+            
+    elif arguments.bicimad_dejar_bici:
+        input_parque = input("¿Cuál es tu parque favorito?: ")
+        bicimad_dejar_bici2 = rp.bicimad_dejar_bici(input_parque,interaction_min_dataset)
+        if input_parque in interaction_min_dataset["Place of interest"].tolist():
+            print(bicimad_dejar_bici2)
+        else:
+            print("Parque mal escrito... busca en Google cachondo")
+            
+    elif arguments.place_description:
+        input_parque = input("¿Cuál es tu parque favorito?: ")
+        place_description2 = rp.place_description(input_parque,interaction_min_dataset)
+        if input_parque in interaction_min_dataset["Place of interest"].tolist():
+            print(place_description2)
+        else:
+            print("Parque mal escrito... busca en Google cachondo")
+            
+    elif arguments.place_barrio:
+        input_parque = input("¿Cuál es tu parque favorito?: ")
+        place_barrio2 = rp.place_barrio(input_parque,interaction_min_dataset)
+        if input_parque in interaction_min_dataset["Place of interest"].tolist():
+            print(place_barrio2)
+        else:
+            print("Parque mal escrito... busca en Google cachondo")
+            
+    elif arguments.place_transport:
+        input_parque = input("¿Cuál es tu parque favorito?: ")
+        place_transport2 = rp.place_transport(input_parque,interaction_min_dataset)
+        if input_parque in interaction_min_dataset["Place of interest"].tolist():
+            print(place_transport2)
+        else:
+            print("Parque mal escrito... busca en Google cachondo")
+
+    elif arguments.bicimad_station_meters:
+        input_parque = input("¿Cuál es tu parque favorito?: ")
+        bicimad_station_meters2 = rp.bicimad_station_meters(input_parque,interaction_min_dataset)
+        if input_parque in interaction_min_dataset["Place of interest"].tolist():
+            print(bicimad_station_meters2)
+        else:
+            print("Parque mal escrito... busca en Google cachondo")
+            
+    elif arguments.email:
+        receiver_email = input("escribe tu email: ")
+        try:
+            rp.wikiparque_email_sender(receiver_email,filename)
+        except:
+            print("Email mal escrito, fallo en Matrix")
+        else:
+            print("email enviado con éxito")  
+        
+    else:
+        print("Comando mal escrito, escribe 'python wikiparque.py -h' para volver a ver el conjunto de comandos")     
+                  
+# Pipeline execution
+
+if __name__ == '__main__':
+    try:
+        main(argument_parser())
+    except:
+        print("LOCOOOO, ¡ESTATE ATENTO! --> Comando mal escrito, escribe 'python wikiparque.py -h' para volver a ver el conjunto de comandos")
+
+
+# In[16]:
+
+
+#arguments.place_description
+
+
+# In[ ]:
+
+
+
+
